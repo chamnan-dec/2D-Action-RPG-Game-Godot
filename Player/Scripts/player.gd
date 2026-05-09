105c5ce93cc5f630a8fc18c93a1534f524414213
chamnan-dec <chamnan.dechkullawit@gmail.com>
2026-05-09 17:26:03 +0700

    Init

---
+++ Player/Scripts/player.gd
@@ -0,0 +1,63 @@
+class_name Player extends CharacterBody2D
+
+var cardinal_direction: Vector2 = Vector2.DOWN
+var direction: Vector2 = Vector2.ZERO
+
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var state_machine: PlayerStateMachine = $StateMachine
+
+signal DirectionChanged(new_direction: Vector2)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	state_machine.Initialize(self)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down"),
+	).normalized()
+	pass
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+
+func SetDirection() -> bool:
+	var new_dir: Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+	if direction.y == 0:
+		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	
+	if new_dir == cardinal_direction:
+		return false
+	DirectionChanged.emit(new_dir)
+	cardinal_direction = new_dir
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+	return true
+	
+func SetState() -> bool:
+	#var new_state: String = "idle" if direction == Vector2.ZERO else "walk"
+	#if new_state == state:
+		#return false
+	#state = new_state
+	return true
+	
+func UpdateAnimation(state: String) -> void:
+	animation_player.play( state + "_" + AnimDirection())
+	pass
+
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
